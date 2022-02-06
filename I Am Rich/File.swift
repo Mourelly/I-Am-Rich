5f42192a24e9f050245800749eb809d3136cb1fe
Yaroslav <70465614+LOTUShkov@users.noreply.github.com>
2022-02-06 17:42:04 +0300

    Add files via upload
    Reload project

---
+++ I Am Rich/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  I Am Rich
+//
+//  Created by Yaroslav on 04.01.2022.
+//
+
+import Foundation
---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yaroslav on 01.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
